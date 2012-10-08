7d9d7c9064763dbd777ce287be501187e295e608
Arien Holthuizen <arienh4@arienh4.net>
2012-10-08 20:28:49 +0200

    Server should work

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+from label import generate, printlatex
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('form.html')
+    
+@app.route('/print', methods=['POST'])
+def printlabel():
+    if(request.form['name'] == '' or request.form['desc'] == ''):
+        return 400
+    name = request.form['name']
+    desc = request.form['desc']
+    owner = request.form['owner']
+    if(owner == ''):
+        owner = 'TechInc'
+    perms = request.form['perms']
+    if(perms == ''):
+        perms = 'Hack it'
+    latex = generate(owner, perms, name, desc)
+    printlatex(latex)
+    return 'Label printed.'
+
+if __name__ == '__main__':
+    app.run(debug=True)
