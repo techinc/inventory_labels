cfda0ce3f60cf4dbac9345112a96981de7487424
Arien Holthuizen <arienh4@arienh4.net>
2012-10-10 16:24:36 +0200

    More form fixes

--- static/js/form.js
+++ static/js/form.js
@@ -1,11 +1,22 @@
-$("#submit").prop("type", "button");
-$("#submit").click(function() {
+$("#alert").hide();
+$("#print").prop("type", "button");
+$("button").click(function() {
+    $('#alert').hide();
+    $(this).button('loading');
 	$.ajax({
-		url: 'print',
-		data: {},
+		url: this.id,
+		data: $('form').serialize(),
 		success: function(data) {
-			$('.result').html(data);
+			$('#results').html(data);
 			alert('Load was performed.');
-		}
+		},
+        error: function(jqXHR, textStatus) {
+            fancyError('Something went wrong: '+textStatus);
+        }
 	});
+    $(this).button('reset');
 });
+
+fancyError = function(message) {
+    $('#alert').html('<strong>Error!</strong> '+message).show();
+}
